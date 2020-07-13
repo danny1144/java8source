382605e67525d964258da1d408acb98bb00ebb05
xiaoping.zhong.ext <xiaoping.zhong.ext@siemens.com>
2020-07-13 17:36:13 +0800

    hashmap

--- src/com/learn/source/java/util/HashMap.java
+++ src/com/learn/source/java/util/HashMap.java
@@ -569,15 +569,20 @@ public class HashMap<K,V> extends AbstractMap<K,V>
      */
     final Node<K,V> getNode(int hash, Object key) {
         Node<K,V>[] tab; Node<K,V> first, e; int n; K k;
+        // table已经初始化，长度大于0，且根据hash寻找table中的项也不为空
         if ((tab = table) != null && (n = tab.length) > 0 &&
             (first = tab[(n - 1) & hash]) != null) {
+            // 比较桶中第一个节点
             if (first.hash == hash && // always check first node
                 ((k = first.key) == key || (key != null && key.equals(k))))
                 return first;
+            // 桶中不止一个结点
             if ((e = first.next) != null) {
+                // 为红黑树结点
                 if (first instanceof TreeNode)
                     return ((TreeNode<K,V>)first).getTreeNode(hash, key);
                 do {
+                    // 否则，在链表中查找
                     if (e.hash == hash &&
                         ((k = e.key) == key || (key != null && key.equals(k))))
                         return e;
@@ -628,44 +633,66 @@ public class HashMap<K,V> extends AbstractMap<K,V>
     final V putVal(int hash, K key, V value, boolean onlyIfAbsent,
                    boolean evict) {
         Node<K,V>[] tab; Node<K,V> p; int n, i;
+        // table未初始化或者长度为0，进行扩容，n为桶的个数
         if ((tab = table) == null || (n = tab.length) == 0)
             n = (tab = resize()).length;
         //如果没有hash碰撞则直接插入元素,多线程环境下，这儿可能会出现数据覆盖问题
+        // (n - 1) & hash 确定元素存放在哪个桶中，桶为空，新生成结点放入桶中(此时，这个结点是放在数组中)
         if ((p = tab[i = (n - 1) & hash]) == null)
             tab[i] = newNode(hash, key, value, null);
+        // 桶中已经存在元素
         else {
             Node<K,V> e; K k;
+            // 比较桶中第一个元素的hash值相等，key相等
             if (p.hash == hash &&
                 ((k = p.key) == key || (key != null && key.equals(k))))
+                // 将第一个元素赋值给e，用e来记录
                 e = p;
+            // hash值不相等或key不相等
             else if (p instanceof TreeNode)
+                // 放入树中
                 e = ((TreeNode<K,V>)p).putTreeVal(this, tab, hash, key, value);
             else {
                 for (int binCount = 0; ; ++binCount) {
+                    // 到达链表的尾部
                     if ((e = p.next) == null) {
+                        // 在尾部插入新结点
                         p.next = newNode(hash, key, value, null);
                         if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
+                            // 结点数量达到阈值，调用treeifyBin()做进一步判断是否转为红黑树
                             treeifyBin(tab, hash);
+                        // 跳出循环
                         break;
                     }
+                    // 判断链表中结点的key值与插入的元素的key值是否相等
                     if (e.hash == hash &&
                         ((k = e.key) == key || (key != null && key.equals(k))))
+                        // 相等，跳出循环
                         break;
+                    // 用于遍历桶中的链表，与前面的e = p.next组合，可以遍历链表
                     p = e;
                 }
             }
+            // 表示在桶中找到key值、hash值与插入元素相等的结点
             if (e != null) { // existing mapping for key
+                // 记录e的value
                 V oldValue = e.value;
+                // onlyIfAbsent为false或者旧值为null
                 if (!onlyIfAbsent || oldValue == null)
+                    //用新值替换旧值
                     e.value = value;
+                // 访问后回调
                 afterNodeAccess(e);
+                // 返回旧值
                 return oldValue;
             }
         }
+        // 结构性修改
         ++modCount;
         //threshold：扩容的阈值，等于 capacity * loadFactor
         if (++size > threshold)
             resize();
+        // 插入后回调
         afterNodeInsertion(evict);
         return null;
     }
